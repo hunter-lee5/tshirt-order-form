e265df69a21a65f829c49b68e341b2aa64e3e050
hunter-lee5 <hunterlee0702@gmail.com>
2025-09-20 20:47:01 -0500

    Adding Contact Info/Payment Type. Moving script to seperate .js file.

---
+++ script.js
@@ -0,0 +1,44 @@
+function generateItemQuestions() {
+    const count = parseInt(document.getElementById("itemCount").value);
+    const container = document.getElementById("itemQuestions");
+    container.innerHTML = ''; // Clear previous questions
+
+    if (isNaN(count) || count < 1) {
+        alert("Please enter a number greater than 1");
+        return;
+    }
+
+    for (let i = 1; i <= count; i++) {
+        const group = document.createElement('div');
+        group.className = 'item-group';
+        group.innerHTML = `
+        <h3>Item ${i}</h3>
+        <label>Type of Shirt:
+            <select name=item${i}_type" required>
+            <option value="">Select Type</option>
+            <option>T-Shirt</option>
+            <option>Sweatshirt</option>
+            </select>
+        <label><br>
+        <label>Size:
+            <select name="item${i}_size" required>
+            <option value="">Select Size</option>
+            <option>Small</option>
+            <option>Medium</option>
+            <option>Large</option>
+            <option>XL</option>
+            <option>XXL</option>
+            <option>XXXL</option>
+            <option>XXXXL</option>
+            </select>
+        </label><br>
+        <label>Color:
+            <input type="text" name="item${i}_color" required>
+        </label><br>
+        <label>Style:
+            <input type="text" name="item${i}_style" required>
+        </label><br>
+        `;
+        container.appendChild(group);
+    }
+}
